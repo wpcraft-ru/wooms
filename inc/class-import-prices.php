e8ca27e013d4a8b9333584c003252878a2bd24c7
Artem Abramovich <artikus.sol@gmail.com>
2018-06-15 23:37:18 +0400

    Update code. Added additional description for setting. Added check for sale prices. Ticket #62

--- inc/class-import-prices.php
+++ inc/class-import-prices.php
@@ -1,80 +1,78 @@
 <?php
+
 /**
  * Select specific price is setup
  */
-class WooMS_Import_Prices
-{
-  public function __construct()
-  {
-    add_filter('wooms_product_price', array($this, 'chg_price'), 10, 2);
-    add_action( 'admin_init', array($this, 'settings'), $priority = 100, $accepted_args = 1 );
-  }
-
-  /**
-   * Update prices for product
-   * Check specifiec price and replace if isset price
-   */
-  public function chg_price( $price, $data )
-  {
-    $price_name = get_option('wooms_price_id');
-
-    if(empty($price_name)){
-      return $price;
-    } else {
-      $price_value = $this->get_value_for_price_type($data);
-      if(empty($price_value)){
-        return $price;
-      } else {
-        return $price_value;
-      }
-    }
-  }
-
-  /**
-  * Get specific price
-  * Return 0 or value price
-  */
-  function get_value_for_price_type($data)
-  {
-    $price_name = get_option('wooms_price_id');
-    if(empty($price_name)){
-      return 0;
-    }
-
-    $price_value = 0;
-    foreach($data["salePrices"] as $price){
-
-      if($price["priceType"] == $price_name){
-        $price_value = $price["value"];
-      }
-    }
-
-    if(empty($price_value)){
-      return 0;
-    } else {
-      return $price_value;
-    }
-  }
-
-  /**
-  * Add settings
-  */
-  function settings(){
-    register_setting('mss-settings', 'wooms_price_id');
-    add_settings_field(
-      $id = 'wooms_price_id',
-      $title = 'Тип Цены',
-      $callback = [$this, 'display_field_wooms_price_id'],
-      $page = 'mss-settings',
-      $section = 'woomss_section_other'
-    );
-  }
-
-  function display_field_wooms_price_id(){
-    $id = 'wooms_price_id';
-    printf('<input type="text" name="%s" value="%s" />', $id, sanitize_text_field(get_option($id)));
-    echo '<p><small>Укажите наименование цены, если нужно выбрать специальный тип цен. Система будет проверять такой тип цены и если он указан то будет подставлять его вместо базового.</small></p>';
-  }
-
+class WooMS_Import_Prices {
+	/**
+	 * WooMS_Import_Prices constructor.
+	 */
+	public function __construct() {
+		add_filter( 'wooms_product_price', array( $this, 'chg_price' ), 10, 2 );
+		add_action( 'admin_init', array( $this, 'settings' ), $priority = 100, $accepted_args = 1 );
+	}
+	
+	/**
+	 * Update prices for product
+	 * Check specifiec price and replace if isset price
+	 */
+	public function chg_price( $price, $data ) {
+		$price_name = get_option( 'wooms_price_id' );
+		
+		if ( empty( $price_name ) || 'Распродажа' === $price_name ) {
+			return $price;
+		} else {
+			$price_value = $this->get_value_for_price_type( $data );
+			if ( empty( $price_value ) ) {
+				return $price;
+			} else {
+				return $price_value;
+			}
+		}
+	}
+	
+	/**
+	 * Get specific price
+	 * Return 0 or value price
+	 */
+	public function get_value_for_price_type( $data ) {
+		$price_name = get_option( 'wooms_price_id' );
+		if ( empty( $price_name ) ) {
+			return 0;
+		}
+		
+		$price_value = 0;
+		foreach ( $data["salePrices"] as $price ) {
+			
+			if ( $price["priceType"] == $price_name ) {
+				$price_value = $price["value"];
+			}
+		}
+		
+		if ( empty( $price_value ) ) {
+			return 0;
+		} else {
+			return $price_value;
+		}
+	}
+	
+	/**
+	 * Add settings
+	 */
+	public function settings() {
+		register_setting( 'mss-settings', 'wooms_price_id' );
+		add_settings_field( $id = 'wooms_price_id', $title = 'Тип Цены', $callback = [
+			$this,
+			'display_field_wooms_price_id',
+		], $page = 'mss-settings', $section = 'woomss_section_other' );
+	}
+	
+	public function display_field_wooms_price_id() {
+		$id = 'wooms_price_id';
+		printf( '<input type="text" name="%s" value="%s" />', $id, sanitize_text_field( get_option( $id ) ) );
+		echo '<p><small>Укажите наименование цены, если нужно выбрать специальный тип цен. Система будет проверять такой тип цены и если он указан то будет подставлять его вместо базового.</small></p>';
+		echo '<p><small>Для добавления распродажных цен укажите в поле слово <strong>Распродажа</strong>. <strong>Внимание! В МойСклад тип расродажных цен должен называться точно также - Распродажа</strong></small></p>';
+	}
 }
+
 new WooMS_Import_Prices;
--- inc/class-import-products.php
+++ inc/class-import-products.php
@@ -4,152 +4,167 @@
  * Import products from MoySklad
  */
 class woomss_tool_products_import {
+	
+	/**
+	 * woomss_tool_products_import constructor.
+	 */
+	public function __construct() {
+		//do_action('wooms_product_import_row', $value, $key, $data);
+		add_action( 'wooms_product_import_row', [ $this, 'load_data' ], 10, 3 );
+	}
+	
+	
+	/**
+	 * Load data and set product type simple
+	 *
+	 * @param $value
+	 * @param $key
+	 * @param $data
+	 */
+	public function load_data( $value, $key, $data ) {
+		
+		if ( ! empty( $value['archived'] ) ) {
+			return;
+		}
+		
+		if ( empty( get_option( 'wooms_use_uuid' ) ) ) {
+			if ( empty( $value['article'] ) ) {
+				return;
+			}
+		}
+		
+		if ( ! empty( $value['article'] ) ) {
+			$product_id = wc_get_product_id_by_sku( $value['article'] );
+		} else {
+			$product_id = '';
+		}
+		
+		if ( intval( $product_id ) ) {
+			$this->update_product( $product_id, $value );
+		} elseif ( $product_id = $this->get_product_id_by_uuid( $value['id'] ) ) {
+			$this->update_product( $product_id, $value );
+		} else {
+			$product_id = $this->add_product( $value );
+			$this->update_product( $product_id, $value );
+		}
+		
+		do_action( 'wooms_product_update', $product_id, $value, $data );
+		
+	}
+	
+	/**
+	 * Update product from source data
+	 *
+	 * @param $product_id var id product
+	 * @param $data_of_source var data of source from MoySklad
+	 *
+	 */
+	public function update_product( $product_id, $data_of_source ) {
+		
+		wp_set_object_terms( $product_id, 'simple', 'product_type', false );
+		
+		$product = wc_get_product( $product_id );
+		
+		//save data of source
+		$now = date( "Y-m-d H:i:s" );
+		update_post_meta( $product_id, 'wooms_data_of_source', print_r( $data_of_source, true ) );
+		
+		//Set session id for product
+		if ( $session_id = get_option( 'wooms_session_id' ) ) {
+			update_post_meta( $product_id, 'wooms_session_id', $session_id );
+		}
+		
+		//the time stamp for database cleanup by cron
+		update_post_meta( $product_id, 'wooms_updated_timestamp', $now );
+		
+		update_post_meta( $product_id, 'wooms_id', $data_of_source['id'] );
+		
+		//update title
+		if ( isset( $data_of_source['name'] ) and $data_of_source['name'] != $product->get_title() ) {
+			if ( ! empty( get_option( 'wooms_replace_title' ) ) ) {
+				$product->set_name( $data_of_source['name'] );
+			}
+		}
+		
+		//update description
+		if ( isset( $data_of_source['description'] ) and empty( $product->get_description() ) ) {
+			$product->set_description( $data_of_source['description'] );
+		}
+		
+		//Price Retail 'salePrices'
+		if ( isset( $data_of_source['salePrices'][0]['value'] ) ) {
+			$price_source = floatval( $data_of_source['salePrices'][0]['value'] );
+			$price        = apply_filters( 'wooms_product_price', $price_source, $data_of_source );
+			
+			$price = $price / 100;
+			
+			$product->set_price( $price );
+			$product->set_regular_price( $price );
+		}
+		
+		$product->set_stock_status( 'instock' );
+		$product->set_manage_stock( 'no' );
+		
+		$product->set_status( 'publish' );
+		$product->save();
+		
+	}
+	
+	/**
+	 * Product Check
+	 *
+	 * @param $uuid
+	 *
+	 * @return bool
+	 */
+	public function get_product_id_by_uuid( $uuid ) {
+		
+		$posts = get_posts( 'post_type=product&meta_key=wooms_id&meta_value=' . $uuid );
+		
+		if ( empty( $posts[0]->ID ) ) {
+			return false;
+		} else {
+			return $posts[0]->ID;
+		}
+	}
+	
+	/**
+	 * Add product from source data
+	 *
+	 * @param $data_of_source var data of source from MoySklad
+	 *
+	 * @return return bool - true or false if updated
+	 */
+	public function add_product( $data_source ) {
+		
+		// $product = new WC_Product_Simple();
+		$post_data = array(
+			'post_type'   => 'product',
+			'post_title'  => wp_filter_post_kses( $data_source['name'] ),
+			'post_status' => 'draft',
+		);
+		
+		if ( ! apply_filters( 'wooms_add_product', true, $data_source ) ) {
+			return false;
+		}
+		
+		// Вставляем запись в базу данных
+		$post_id = wp_insert_post( $post_data );
+		
+		// $product = wc_get_product($post_id);
+		
+		if ( empty( $post_id ) ) {
+			return false;
+		}
+		
+		update_post_meta( $post_id, $meta_key = 'wooms_id', $meta_value = $data_source['id'] );
+		
+		if ( isset( $data_source['article'] ) ) {
+			update_post_meta( $post_id, $meta_key = '_sku', $meta_value = $data_source['article'] );
+		}
+		
+		return $post_id;
+	}
+}
 
-    function __construct(){
-      //do_action('wooms_product_import_row', $value, $key, $data);
-        add_action('wooms_product_import_row', [$this, 'load_data'], 10, 3);
-    }
-
-
-    function load_data($value, $key, $data){
-
-      if( ! empty($value['archived'])){
-        return;
-      }
-
-      if( empty( get_option('wooms_use_uuid') ) ){
-        if( empty($value['article'])){
-          return;
-        }
-      }
-
-      if( ! empty($value['article'])){
-        $product_id = wc_get_product_id_by_sku($value['article']);
-      } else {
-        $product_id = '';
-      }
-
-      if( intval($product_id) ){
-        $this->update_product($product_id, $value);
-      } elseif( $product_id = $this->get_product_id_by_uuid($value['id']) ) {
-        $this->update_product($product_id, $value);
-      } else {
-        $product_id = $this->add_product($value);
-        $this->update_product($product_id, $value);
-      }
-
-      do_action('wooms_product_update', $product_id, $value, $data);
-
-    }
-
-
-    function get_product_id_by_uuid($uuid){
-
-      $posts = get_posts('post_type=product&meta_key=wooms_id&meta_value='.$uuid);
-
-      if(empty($posts[0]->ID)){
-        return false;
-      } else {
-        return $posts[0]->ID;
-      }
-    }
-
-    /**
-     * Add product from source data
-     *
-     * @param $data_of_source var data of source from MoySklad
-     * @return return bool - true or false if updated
-     */
-    function add_product($data_source){
-
-        // $product = new WC_Product_Simple();
-        $post_data = array(
-          'post_type' => 'product',
-          'post_title'    => wp_filter_post_kses( $data_source['name'] ),
-          'post_status'   => 'draft'
-        );
-
-        if( ! apply_filters('wooms_add_product', true, $data_source)){
-          return false;
-        }
-
-        // Вставляем запись в базу данных
-        $post_id = wp_insert_post( $post_data );
-
-        // $product = wc_get_product($post_id);
-
-        if(empty($post_id)){
-          return false;
-        }
-
-        update_post_meta( $post_id, $meta_key = 'wooms_id', $meta_value = $data_source['id'] );
-
-        if( isset($data_source['article']) ){
-            update_post_meta( $post_id, $meta_key = '_sku', $meta_value = $data_source['article'] );
-        }
-
-        return $post_id;
-    }
-
-    /**
-     * Update product from source data
-     *
-     * @param $product_id var id product
-     * @param $data_of_source var data of source from MoySklad
-     * @return return bool - true or false if updated
-     */
-    function update_product($product_id, $data_of_source){
-
-        wp_set_object_terms( $product_id, 'simple', 'product_type', false );
-
-        $product = wc_get_product($product_id);
-
-        //save data of source
-        $now = date("Y-m-d H:i:s");
-        update_post_meta($product_id, 'wooms_data_of_source', print_r($data_of_source, true));
-
-        //Set session id for product
-        if($session_id = get_option('wooms_session_id')){
-          update_post_meta($product_id, 'wooms_session_id', $session_id);
-        }
-
-        //the time stamp for database cleanup by cron
-        update_post_meta($product_id, 'wooms_updated_timestamp', $now);
-
-        update_post_meta($product_id, 'wooms_id', $data_of_source['id']);
-
-        //update title
-        if( isset($data_of_source['name']) and $data_of_source['name'] != $product->get_title() ){
-          if( ! empty(get_option('wooms_replace_title'))){
-            $product->set_name( $data_of_source['name'] );
-          }
-        }
-
-        //update description
-        if( isset($data_of_source['description']) and empty($product->get_description()) ){
-          $product->set_description($data_of_source['description']);
-        }
-
-
-        //Price Retail 'salePrices'
-        if(isset($data_of_source['salePrices'][0]['value'])){
-          $price_source = floatval($data_of_source['salePrices'][0]['value']);
-          $price = apply_filters('wooms_product_price', $price_source, $data_of_source);
-
-          $price = $price/100;
-
-          $product->set_price( $price );
-          $product->set_regular_price( $price );
-
-        }
-
-        $product->set_stock_status('instock');
-        $product->set_manage_stock('no');
-
-        $product->set_status('publish');
-        $product->save();
-    }
-
-
-
-} new woomss_tool_products_import;
+new woomss_tool_products_import;
