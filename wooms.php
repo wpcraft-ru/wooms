fcf6d5b80566010aa321438b16233beabcadee9a
Artem Abramovich <artikus.sol@gmail.com>
2018-07-16 23:21:15 +0400

    Alternative way select category, second developments. Ticket #28

--- wooms.php
+++ wooms.php
@@ -9,7 +9,7 @@
  * Developer URI: https://wpcraft.ru/
  * Text Domain: wooms
  * Domain Path: /languages
- * Version: 2.0.6
+ * Version: 2.0.7
  *
  * WC requires at least: 3.0
  * WC tested up to: 3.3.3
