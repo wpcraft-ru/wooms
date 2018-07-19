9b606afd718691bfa4e0130b9c889f7925fa6e56
Artem Abramovich <artikus.sol@gmail.com>
2018-07-19 12:44:51 +0400

    Ticket #22. Hide product archive. Commit first

--- inc/class-hide-old-products.php
+++ inc/class-hide-old-products.php
@@ -11,29 +11,40 @@ class WooMS_Hide_Old_Products
     add_action( 'wooms_cron_clear_old_products_walker', array($this, 'cron_starter'));
 
   }
+	
+	function walker() {
+		
+		// do_action('logger_u7', ['tt2', 1]);
+		
+		$session_id = get_option( 'wooms_session_id' );
+		if ( empty( $session_id ) ) {
+			return false;
+		}
+		
+		//Получаем продукты у которых сессия старее чем текущая
+		$args = array(
+			'post_type'    => 'product',
+			'meta_compare' => '!=',
+			'meta_key'     => 'wooms_session_id',
+			'meta_value'   => $session_id,
+			'fields'       => 'ids',
+		);
+		
+		$products = get_posts( $args );
+		
+		//do_action( 'logger_u7', [ 'tt1', $lists ] );
+		
+		foreach ( $products as $product_id ) {
+			$product = wc_get_product( $product_id );
+			
+			$product->set_catalog_visibility('hidden');
+			$product->set_stock_status( 'outofstock' );
 
-  function walker(){
-
-    // do_action('logger_u7', ['tt2', 1]);
-
-    $session_id = get_option('wooms_session_id');
-    if(empty($session_id)){
-      return false;
-    }
-
-    //Получаем продукты у которых сессия старее чем текущая
-    $args = array(
-      'post_type' => 'product',
-      'meta_compare' => '!=',
-      'meta_key' => 'wooms_session_id',
-      'meta_value' => $session_id,
-    );
-
-    $list = get_posts($args);
-
-    //do_action('logger_u7', ['tt1', $list]);
-
-  }
+			//do_action( 'logger_u7', [ 'tt2',$id, $product->get_stock_status(), $product->get_catalog_visibility() ] );
+			$product->save();
+		}
+		
+	}
 
   /**
   * Cron task restart
