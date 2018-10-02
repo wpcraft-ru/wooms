5a2dd3d902ed9b4991cfca9f7b1026a7f520f772
Artem Abramovich <artikus.sol@gmail.com>
2018-10-02 20:12:29 +0400

    Ticket #65. Added update groups

--- inc/class-import-product-categories.php
+++ inc/class-import-product-categories.php
@@ -1,4 +1,5 @@
 <?php
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
@@ -12,6 +13,7 @@ class WooMS_Import_Product_Categories {
 	 * WooMS_Import_Product_Categories constructor.
 	 */
 	public function __construct() {
+		
 		/**
 		 * Use hook: do_action('wooms_product_update', $product_id, $value, $data);
 		 */
@@ -29,6 +31,7 @@ class WooMS_Import_Product_Categories {
 	 * @param $data
 	 */
 	public function load_data( $product_id, $value, $data ) {
+		
 		//Если опция отключена - пропускаем обработку
 		if ( get_option( 'woomss_categories_sync_enabled' ) ) {
 			return;
@@ -55,64 +58,76 @@ class WooMS_Import_Product_Categories {
 	 * @return bool|int|mixed
 	 */
 	public function update_category( $url ) {
+		
 		$data = wooms_request( $url );
-		//do_action("logger_u7", [ $data, $url] );
+
 		if ( $term_id = $this->check_term_by_ms_id( $data['id'] ) ) {
 			
 			do_action( 'wooms_update_category', $term_id );
 			
-			return $term_id;
-
-		} else {
-		
-		$args = array();
-		
-		$term_new = array(
-			'wooms_id' => $data['id'],
-			'name'     => $data['name'],
-			'archived' => $data['archived'],
-		);
-		
-		if ( isset( $data['productFolder']['meta']['href'] ) ) {
-			$url_parent = $data['productFolder']['meta']['href'];
-			if ( $term_id_parent = $this->update_category( $url_parent ) ) {
-				$args['parent'] = intval( $term_id_parent );
+			if ( isset( $data['productFolder']['meta']['href'] ) ) {
+				$url_parent = $data['productFolder']['meta']['href'];
+				if ( $term_id_parent = $this->update_category( $url_parent ) ) {
+					$parent = isset( $term_id_parent ) ? intval( $term_id_parent ) : 0;
+				}
 			}
-		}
-		
-		$url_parent = isset( $data['productFolder']['meta']['href'] ) ? $data['productFolder']['meta']['href'] : '';
-		$path_name  = isset( $data['pathName'] ) ? $data['pathName'] : null;
+			$term = wp_update_term( $term_id, 'product_cat', array( 'parent' => $parent ) );
 
-			if ( apply_filters( 'wooms_skip_categories', true, $url_parent, $path_name ) ) {
-				$term = wp_insert_term( $term_new['name'], $taxonomy = 'product_cat', $args );
-			}
-
-
-		
-		//wp_suspend_cache_addition( $was_suspended );
-		
-		if ( isset( $term->errors["term_exists"] ) ) {
-			$term_id = intval( $term->error_data['term_exists'] );
-			if ( empty( $term_id ) ) {
+			if ( is_array( $term ) && ! empty( $term["term_id"] ) ) {
+				return $term["term_id"];
+			} else {
 				return false;
 			}
-		} elseif ( isset( $term->term_id ) ) {
-			$term_id = $term->term_id;
-		} elseif ( is_array( $term ) && ! empty( $term["term_id"] ) ) {
-			$term_id = $term["term_id"];
+			
 		} else {
-			return false;
-		}
-		
-		update_term_meta( $term_id, 'wooms_id', $term_new['wooms_id'] );
-		
-		if ( $session_id = get_option( 'wooms_session_id' ) ) {
-			update_term_meta( $term_id, 'wooms_session_id', $session_id );
-		}
-		
-		do_action( 'wooms_add_category', $term, $url_parent, $path_name );
-		
-		return $term_id;
+			
+			$args = array();
+			
+			$term_new = array(
+				'wooms_id' => $data['id'],
+				'name'     => $data['name'],
+				'archived' => $data['archived'],
+			);
+			
+			if ( isset( $data['productFolder']['meta']['href'] ) ) {
+				$url_parent = $data['productFolder']['meta']['href'];
+				if ( $term_id_parent = $this->update_category( $url_parent ) ) {
+					$args['parent'] = intval( $term_id_parent );
+				}
+			}
+			
+			$url_parent = isset( $data['productFolder']['meta']['href'] ) ? $data['productFolder']['meta']['href'] : '';
+			$path_name  = isset( $data['pathName'] ) ? $data['pathName'] : null;
+			
+			if ( apply_filters( 'wooms_skip_categories', true, $url_parent, $path_name ) ) {
+				
+				$term = wp_insert_term( $term_new['name'], $taxonomy = 'product_cat', $args );
+			}
+			
+			//wp_suspend_cache_addition( $was_suspended );
+			
+			if ( isset( $term->errors["term_exists"] ) ) {
+				$term_id = intval( $term->error_data['term_exists'] );
+				if ( empty( $term_id ) ) {
+					return false;
+				}
+			} elseif ( isset( $term->term_id ) ) {
+				$term_id = $term->term_id;
+			} elseif ( is_array( $term ) && ! empty( $term["term_id"] ) ) {
+				$term_id = $term["term_id"];
+			} else {
+				return false;
+			}
+			
+			update_term_meta( $term_id, 'wooms_id', $term_new['wooms_id'] );
+			
+			if ( $session_id = get_option( 'wooms_session_id' ) ) {
+				update_term_meta( $term_id, 'wooms_session_id', $session_id );
+			}
+			
+			do_action( 'wooms_add_category', $term, $url_parent, $path_name );
+			
+			return $term_id;
 		}
 		
 	}
@@ -214,6 +229,7 @@ class WooMS_Import_Product_Categories {
 	 * @param $term
 	 */
 	public function add_data_category( $term ) {
+		
 		$meta_data = get_term_meta( $term->term_id, 'wooms_id', true );
 		if ( ! $meta_data ) {
 			$meta_data = '';
@@ -259,11 +275,13 @@ class WooMS_Import_Product_Categories {
 	
 	//Display field
 	public function display_option_categories_sync_enabled() {
+		
 		$option = 'woomss_categories_sync_enabled';
 		printf( '<input type="checkbox" name="%s" value="1" %s />', $option, checked( 1, get_option( $option ), false ) );
 		?>
 		<small>Если включить опцию, то при обновлении продуктов категории не будут учтываться в соответствии с группами МойСклад.</small>
 		<?php
+		
 	}
 }
 
