de8abb9f2620c7e00c10b84fc2db10cb3fb5aff9
Artem Abramovich <artikus.sol@gmail.com>
2018-10-04 00:52:35 +0400

    Update version and readme

--- wooms.php
+++ wooms.php
@@ -16,7 +16,7 @@
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Version: 2.1.0
- * WooMS XT Latest: 1.8.5
+ * WooMS XT Latest: 1.8.6
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
