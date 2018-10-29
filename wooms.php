b48e33905e405c151bdbda1ab4f567e5a6d7db47
Artem Abramovich <artikus.sol@gmail.com>
2018-10-29 14:35:15 +0400

    Update version and readme

--- wooms.php
+++ wooms.php
@@ -10,12 +10,12 @@
  * Text Domain: wooms
  * Domain Path: /languages
  * WC requires at least: 3.0
- * WC tested up to: 3.4.7
+ * WC tested up to: 3.5.0
  * PHP requires at least: 5.6
  * WP requires at least: 4.8
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Version: 2.1.1
+ * Version: 2.1.2
  * WooMS XT Latest: 1.8.8
  */
 
