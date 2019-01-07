c7d0a140315595b859dee7fa231d3be75681902c
R2 D2 <uptimizt@gmail.com>
2019-01-07 21:25:45 +0300

    bump ver and changelog

--- wooms.php
+++ wooms.php
@@ -15,8 +15,8 @@
  * WP requires at least: 4.8
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Version: 3.7
- * WooMS XT Latest: 3.7
+ * Version: 3.8
+ * WooMS XT Latest: 3.8
  */
 
 // Exit if accessed directly
