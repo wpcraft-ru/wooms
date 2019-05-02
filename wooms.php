2486ccc15e1529dc0993d158819e84e74fa53345
R2 D2 <uptimizt@gmail.com>
2019-05-02 19:32:51 +0300

    v5.2 rc1

--- wooms.php
+++ wooms.php
@@ -15,8 +15,8 @@
  * WP requires at least: 4.8
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Version: 5.1
- * WooMS XT Latest: 5.1
+ * Version: 5.2
+ * WooMS XT Latest: 5.2
  */
 
 // Exit if accessed directly
