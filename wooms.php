0b2b7173149b32445ee044807c84b86f6a5228d0
uptimizt <uptimizt@gmail.com>
2020-04-03 21:43:07 +0300

    version bump

--- wooms.php
+++ wooms.php
@@ -17,8 +17,8 @@
  * Tested up to: 5.6
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Version: 6.3
- * WooMS XT Latest: 6.3
+ * Version: 6.4
+ * WooMS XT Latest: 6.4
  */
 
 // Exit if accessed directly
