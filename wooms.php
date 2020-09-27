90e47eee36349051c9ca11522111a43818122ba0
r2 d2 <uptimizt@gmail.com>
2020-09-27 16:59:53 +0300

    rc 7.14

--- wooms.php
+++ wooms.php
@@ -16,8 +16,8 @@
  * PHP requires at least: 5.6
  * WP requires at least: 5.0
  * Tested up to: 5.6
- * WooMS XT Latest: 7.12
- * Version: 7.12
+ * WooMS XT Latest: 7.14
+ * Version: 7.14
  */
 
 // Exit if accessed directly
