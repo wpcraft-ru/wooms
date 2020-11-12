2198961cedec18ccf7e0d2fa64e29e70a66660bf
r2 d2 <uptimizt@gmail.com>
2020-11-12 21:20:57 +0300

    RC 8.0

--- wooms.php
+++ wooms.php
@@ -12,7 +12,7 @@
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * WC requires at least: 3.6
- * WC tested up to: 4.2
+ * WC tested up to: 4.7.0
  * PHP requires at least: 5.6
  * WP requires at least: 5.0
  * Tested up to: 5.6
