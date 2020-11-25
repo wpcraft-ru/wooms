ad1f69f173302bd083dbfc235fbfb86cd70978e9
r2 d2 <uptimizt@gmail.com>
2020-11-25 19:47:37 +0300

    rc 8.1

--- wooms.php
+++ wooms.php
@@ -16,8 +16,8 @@
  * PHP requires at least: 5.6
  * WP requires at least: 5.0
  * Tested up to: 5.6
- * WooMS XT Latest: 8.0
- * Version: 8.0
+ * WooMS XT Latest: 8.1
+ * Version: 8.1
  */
 
 // Exit if accessed directly
