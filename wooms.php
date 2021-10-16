52f82945e48faa4426c0cce8b1c2fa08818b87a5
Anatoly Yumashev <uptimizt@gmail.com>
2021-10-16 18:25:28 +0300

    version bump

--- wooms.php
+++ wooms.php
@@ -11,13 +11,13 @@
  * Domain Path: /languages
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * WC requires at least: 4.0
- * WC tested up to: 5.7.0
  * PHP requires at least: 5.6
+ * WC requires at least: 5.0
+ * WC tested up to: 5.8
  * WP requires at least: 5.0
  * Tested up to: 5.8.1
- * WooMS XT Latest: 8.2
- * Version: 8.2
+ * WooMS XT Latest: 8.4
+ * Version: 8.4
  */
 
 // Exit if accessed directly
