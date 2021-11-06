63444a7d54b99478c33163684c5d9184e35decc0
Anatoly Yumashev <uptimizt@gmail.com>
2021-11-06 15:39:11 +0300

    plugin meta bumped

--- wooms.php
+++ wooms.php
@@ -11,12 +11,14 @@
  * Domain Path: /languages
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * 
  * PHP requires at least: 5.6
  * WC requires at least: 5.0
  * WC tested up to: 5.8
  * WP requires at least: 5.0
  * Tested up to: 5.8.1
  * WooMS XT Latest: 8.4
+ * 
  * Version: 8.4
  */
 
