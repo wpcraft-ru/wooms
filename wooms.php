efa06c42d39bc9c90fd7e1c64a9395d3e324c8f0
Anatoly Yumashev <1852897+uptimizt@users.noreply.github.com>
2022-11-25 19:14:15 +0300

    add support - wp-plugin-dependencies

--- wooms.php
+++ wooms.php
@@ -12,6 +12,7 @@
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
+ * Requires Plugins: woocommerce
  * PHP requires at least: 5.6
  * WP requires at least: 5.0
  * Tested up to: 5.9
