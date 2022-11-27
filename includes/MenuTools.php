972bb092c9f4d0fe597937d27e97abac801203b3
Anatoly Yumashev <1852897+uptimizt@users.noreply.github.com>
2022-11-27 14:24:53 +0300

    tests and code style improve

--- includes/MenuTools.php
+++ includes/MenuTools.php
@@ -44,28 +44,22 @@ class MenuTools {
    * Display UI
    */
   public static function display_ui()
-  { ?>
-<h1>Управление МойСклад</h1>
+  {
+    printf('<h1>%s</h1>', 'Управление МойСклад');
+    $items = [
+      '<a style="color:green;" href="https://github.com/wpcraft-ru/wooms/wiki/GettingStarted" target="_blank">
+          <strong>Начало работы</strong>
+      </a>',
+      sprintf('<a href="%s">Настройки</a>', admin_url("admin.php?page=mss-settings") ),
+      '<a href="https://online.moysklad.ru/app/" target="_blank">Вход в МойСклад</a>',
+      sprintf('<a href="%s">Диагностика проблем</a>', admin_url("site-health.php") ),
+      '<a href="https://wpcraft.ru/wordpress/hosting/" target="_blank">Рекомендуемые хостинги</a>',
+      '<a href="https://wpcraft.ru/wordpress-woocommerce-mentoring/" target="_blank">Сопровождение магазинов и консалтинг</a>',
+    ];
 
-<p>
-    <a style="color:green;" href="https://github.com/wpcraft-ru/wooms/wiki/GettingStarted"
-        target="_blank"><strong>Начало
-            работы</strong></a>
-    <span> | </span>
-    <a href="<?php echo admin_url('admin.php?page=mss-settings') ?>">Настройки</a>
-    <span> | </span>
-    <a href="https://online.moysklad.ru/app/" target="_blank">Вход в МойСклад</a>
-    <span> | </span>
-    <a href="<?php echo admin_url('site-health.php') ?>">Диагностика проблем</a>
-    <span> | </span>
-    <a href="https://wpcraft.ru/wordpress/hosting/" target="_blank">Рекомендуемые хостинги</a>
-    <span> | </span>
-    <a href="https://wpcraft.ru/wordpress-woocommerce-mentoring/" target="_blank">Сопровождение магазинов и консалтинг</a>
+    printf( '<p>%s</p>', implode('<span> | </span>', $items) );
 
-</p>
-
-<?php
-    if(empty($_GET['a'])){
+    if( empty($_GET['a']) ){
       do_action('woomss_tool_actions_btns');
 
       do_action('wooms_tools_sections');
--- tests/tdd/ProductsTests.php
+++ tests/tdd/ProductsTests.php
@@ -5,14 +5,45 @@
  * @package Unit_Test_Plugin
  */
 
-use PHPUnit\Framework\TestCase;
+use WooMS\Products;
+use function WooMS\Products\get_product_id_by_uuid;
+use function WooMS\Products\walker;
 
 /**
  * Sample test case.
  */
-class Products extends TestCase {
+class ProductsTests extends WP_UnitTestCase {
 
   private $product_id = null;
+
+
+  public function test_walker(){
+    $data = [
+      'stop_manual' => 0,
+      'timestamp' => '20221127110457',
+      'finish' => NULL,
+      'wooms_end_timestamp' => '2022-11-27 11:02:13',
+      'session_id' => '20221127110457',
+      'end_timestamp' => 0,
+      'count' => 0,
+      'query_arg' => array ( 'offset' => 0, 'limit' => '20', ),
+    ];
+
+    $result = walker();
+
+    var_dump($result);
+
+  }
+
+  /**
+   * check product by UUID
+   */
+  public function test_checkProductByUuid(){
+    $json = $this->getJsonForSimpleProduct_code00045();
+    $product_id = get_product_id_by_uuid($json['id']);
+		$this->assertIsInt( $product_id );
+  }
+
 	/**
 	 * Product Loaded by JSON and get ID
 	 */
@@ -30,13 +61,6 @@ class Products extends TestCase {
 	}
 
 
-  public function test_CanStart() {
-    $can_start = wooms_can_start();
-
-		$this->assertTrue( $can_start );
-	}
-
-
   function getJsonForSimpleProduct_code00045(){
     $rows = $this->readJsonApiFromFile();
     foreach($rows as $row){
@@ -48,7 +72,7 @@ class Products extends TestCase {
   }
 
   function readJsonApiFromFile(){
-    $strJsonFileContents = file_get_contents(__DIR__ . "/json/rows.json");
+    $strJsonFileContents = file_get_contents(__DIR__ . "/../json/rows.json");
     $data = json_decode($strJsonFileContents, true);
     return $data;
   }
@@ -67,7 +91,4 @@ class Products extends TestCase {
       //TBD
       parent::tearDown();
   }
-
-
-
 }
