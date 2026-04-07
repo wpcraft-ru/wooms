24076c39e325a91ea57a9dfe87e9d474ac21146d
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2026-04-07 15:03:26 +0300

    bug with wp-env.json

--- wooms.php
+++ wooms.php
@@ -11,7 +11,6 @@
  * Domain Path: /languages
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Requires Plugins: woocommerce
  *
  * PHP requires at least: 7.0
  * WP requires at least: 5.0
